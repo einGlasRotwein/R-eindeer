8ee386e7cd428ea22281b5240caee02b7233b722
Juli Tkotz <juliane.tkotz@gmail.com>
2018-12-04 23:10:07 +0100

    Update reindeer.R
    Added more coat colours. Added highlight colours. Added hoove colours. Added eye colours. Added eye details.
    Labelled buttons with numbers instead of input values (colour names) - for now.

--- reindeer.R
+++ reindeer.R
@@ -7,7 +7,26 @@ ui <- fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-      radioButtons("check1","coat colour", choices = c("grey","brown"))
+      radioButtons("coat_colour","coat colour", 
+                   choiceNames = c("1", "2", "3", "4", "5", "6", "7"), 
+                   choiceValues = c("grey", "dark_grey", "black", "brown", "sorrel", "gold", 
+                                    "cream"), 
+                   inline = TRUE),
+      
+      radioButtons("highlights","highlights", 
+                   choiceNames = c("1", "2", "3", "4"), 
+                   choiceValues = c("none", "dark", "light", "R_blue"), 
+                   inline = TRUE),
+      
+      radioButtons("hooves","hooves", 
+                   choiceNames = c("1", "2", "3"), 
+                   choiceValues = c("dark", "light", "R_blue"), 
+                   inline = TRUE),
+      
+      radioButtons("eye_colour","eye colour", 
+                   choiceNames = c("1", "2", "3"), 
+                   choiceValues = c("light_brown", "dark_brown", "blue"), 
+                   inline = TRUE)
       ),
     
     mainPanel(
@@ -18,18 +37,22 @@ ui <- fluidPage(
 
 server <- function(input,output){
   
-  get_image <- function(type, color) {
-    image_read(file.path(type, paste0(color, ".png")))
+  get_image <- function(type, colour) {
+    image_read(file.path(type, paste0(colour, ".png")))
   }
   
   output$reindeer <- renderImage({
     
     # load the images
-    coat <- get_image("coat", paste0("reindeer_", input$check1))
+    coat <- get_image("coat", paste0("reindeer_", input$coat_colour))
+    highlights <- get_image("highlights", paste0("highlights_", input$highlights))
+    hooves <- get_image("details", paste0("hooves_", input$hooves))
+    eye_colour <- get_image("details", paste0("eye_", input$eye_colour))
+    eye_background <- get_image("details", "eye_and_nostril")
     outline <- get_image("outlines", "reindeer_outline")
     
     # make the reindeer: overlay in order
-    reindeer <- c(coat, outline)
+    reindeer <- c(coat, highlights, hooves, eye_colour, eye_background, outline)
     
     # create a temp file
     tmpfile <- reindeer %>%

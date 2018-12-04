c662fe80cfd24d1e9312cfc745efa4d3f6712934
Juli Tkotz <juliane.tkotz@gmail.com>
2018-12-04 10:31:48 +0100

    overlaying images with magick
    Now the image layers are placed neatly on top of each other. [magick](https://cran.r-project.org/web/packages/magick/vignettes/intro.html) did the trick. Thank you [blondeclover](https://stackoverflow.com/users/8099834/blondeclover) and [Ian Wesley](https://stackoverflow.com/users/7384676/ian-wesley) from [Stackoverflow](https://stackoverflow.com) for helping out!

--- reindeer.R
+++ reindeer.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(magick)
 
 ui <- fluidPage(
   
@@ -10,31 +11,39 @@ ui <- fluidPage(
       ),
     
     mainPanel(
-      imageOutput("reindeer_coat"),
-      imageOutput("reindeer_outline")
+      imageOutput("reindeer")
       )
     )
   )
 
-
 server <- function(input,output){
   
-  getImage <- reactive({
-    list(src = paste0("./coat/reindeer_", input$check1, ".png"),
-         width = 500,
-         height = 500)
-  })
+  get_image <- function(type, color) {
+    image_read(file.path(type, paste0(color, ".png")))
+  }
   
-  output$reindeer_coat <- renderImage({
-    getImage()
-  }, deleteFile = FALSE)
-  
-  output$reindeer_outline <- renderImage({
-    return(list(src = "./outlines/reindeer_outline.png",
-                width = 500,
+  output$reindeer <- renderImage({
+    
+    # load the images
+    coat <- get_image("coat", paste0("reindeer_", input$check1))
+    outline <- get_image("outlines", "reindeer_outline")
+    
+    # make the reindeer: overlay in order
+    reindeer <- c(coat, outline)
+    
+    # create a temp file
+    tmpfile <- reindeer %>%
+      image_mosaic() %>%
+      image_flatten() %>%
+      image_write(tempfile(fileext='jpg'), format = 'jpg')
+    
+    # render the file
+    return(list(src = tmpfile,
                 height = 500,
-                contentType = 'image/png'))
-  }, deleteFile = FALSE)
+                width = 500,
+                alt = "Your reindeer",
+                contentType = "image/jpg"))
+  }, deleteFile = TRUE)
 }
 
 shinyApp(ui = ui, server = server)

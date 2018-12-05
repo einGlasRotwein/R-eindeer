33de1ff8b8bbdb3941f38f931e4ab28c57002da8
Juli Tkotz <juliane.tkotz@gmail.com>
2018-12-05 12:51:41 +0100

    Update reindeer.R
    added pink coat colour, added blanket options

--- reindeer.R
+++ reindeer.R
@@ -8,9 +8,9 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       radioButtons("coat_colour","coat colour", 
-                   choiceNames = c("1", "2", "3", "4", "5", "6", "7"), 
+                   choiceNames = c("1", "2", "3", "4", "5", "6", "7", "8"), 
                    choiceValues = c("grey", "dark_grey", "black", "brown", "sorrel", "gold", 
-                                    "cream"), 
+                                    "cream", "pink"), 
                    inline = TRUE),
       
       radioButtons("highlights","highlights", 
@@ -26,6 +26,14 @@ ui <- fluidPage(
       radioButtons("eye_colour","eye colour", 
                    choiceNames = c("1", "2", "3"), 
                    choiceValues = c("light_brown", "dark_brown", "blue"), 
+                   inline = TRUE),
+      
+      radioButtons("blanket","blanket", 
+                   choiceNames = c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+                                                   "11", "12", "13"), 
+                   choiceValues = c("none", "red", "orange", "yellow", "green", "R_blue", 
+                                    "R_blue_R", "dark_blue", "purple", "pink", "black", "grey", 
+                                    "grey_R"), 
                    inline = TRUE)
       ),
     
@@ -41,7 +49,12 @@ server <- function(input,output){
     image_read(file.path(type, paste0(colour, ".png")))
   }
   
+  
   output$reindeer <- renderImage({
+    
+    # only show blanket outline when a blanket is selected (i.e. load blank layer when blanket
+    # input is "none")
+    blanket_status <- ifelse(input$blanket == "none", "none", "blanket")
     
     # load the images
     coat <- get_image("coat", paste0("reindeer_", input$coat_colour))
@@ -49,10 +62,13 @@ server <- function(input,output){
     hooves <- get_image("details", paste0("hooves_", input$hooves))
     eye_colour <- get_image("details", paste0("eye_", input$eye_colour))
     eye_background <- get_image("details", "eye_and_nostril")
-    outline <- get_image("outlines", "reindeer_outline")
+    outline <- get_image("outlines", "outline_reindeer")
+    blanket <- get_image("blanket", paste0("blanket_", input$blanket))
+    outline_blanket <- get_image("outlines", paste0("outline_", blanket_status))
     
     # make the reindeer: overlay in order
-    reindeer <- c(coat, highlights, hooves, eye_colour, eye_background, outline)
+    reindeer <- c(coat, highlights, hooves, eye_colour, eye_background, outline, blanket,
+                  outline_blanket)
     
     # create a temp file
     tmpfile <- reindeer %>%
